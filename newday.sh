b2058483fcd0d215949cacc466abb7622ec0b721
St33v on now vm <f3rr3t@st33v.com>
2017-07-11 21:53:04 +1000

    can generate yyyy/mm/dd now

--- newday.sh
+++ newday.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/bash
 # Function to add a new directory if required
 
-yr=date +%Y     # 4-digit year
-mo=date +%m
-day=date +%d
-
-:
+function newdaydir {
+	yr=$(date +%Y)     # 4-digit year
+	mo=$(date +%m)
+	day=$(date +%d)
+	if [ ! -d ${yr}/${mo}/${day} ]; then
+		if [ ! -d ${yr}/${mo} ]; then
+			if [ ! -d ${yr} ]; then
+				mkdir ${yr}	
+			fi
+			mkdir ${yr}/${mo}
+		fi
+		mkdir ${yr}/${mo}/${day}
+	fi
+	cd ${yr}/${mo}/${day}
+	echo $PWD
+}
