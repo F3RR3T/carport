5cb7ffe77e10e345e7d8104bb4c78d70bf396ac4
St33v on now vm <f3rr3t@st33v.com>
2017-07-11 22:00:13 +1000

    write pix to $PWD/yyyy/mm/dd/ (untested...)

--- takecarportpix.sh
+++ takecarportpix.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 # takepix.sh  SJP 10 January 2017
 cd /usr/local/bin/
+. newday.sh
 
 picdir="/path/to/my/photos"  # overwritten by sourcing from .picdir.config
 if [ -e picdir.config ]; then
     . picdir.config             # source directory name from local config file
 else echo "picdir.config does not exist"; exit 1
 fi
+
 #echo camherder = $camherder
 picdate=$(date +%Y-%m-%d_%H%M%S)
 artist=carport
 
 cd ${picdir}
+newdaydir
 curl -sO 133.7.0.103/image.jpg
 mv image.jpg $picdate.jpg
 
