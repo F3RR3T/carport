49df99d76173fc619069aa667caff4bfbbd853df
st33v <github@f3rr3t.com>
2018-04-08 17:18:16 +1000

    loopover all cams now

--- takeminicampix.sh
+++ takeminicampix.sh
@@ -1,48 +1,58 @@
 #!/bin/bash
 # takepix.sh  SJP 10 January 2017
 cd /usr/local/bin/
-. newday.sh
+# define newdaydir function and makemovie function
+source newday.sh   
 
 threshold=550 # Used to find photoes that are too dark (using imagemagick identify function)
     # threshold was found by trial and error for this particular camera/scene.
     # Is there a better way?
 picdir="/path/to/my/photos"  # overwritten by sourcing from .picdir.config
-camz="host1 host2 host3 etc" # overwritten by picdir.config
+declare -a camz
+camz=(host1 host2 host3 etc) # overwritten by picdir.config
 if [ -e picdir.config ]; then
-    . picdir.config             # source directory name from local config file
+    source picdir.config             # source directory name from local config file
 else echo "picdir.config does not exist"; exit 1
 fi
 
-if [ ${camz[1]} -eq 'host1' ] ; then
-    echo "Error: camera host(s) not configured in picdir.config'
+if [[ ${camz[0]} == 'host1' ]] ; then
+    echo 'Error: camera host(s) not configured in picdir.config'
     exit 1
 fi
 
+timestamp=$(date +%Y-%m-%d_%H%M%S)
+
 for cam in "${camz[@]}"
 do
-    #echo camherder = $camherder
-    timestamp=$(date +%Y-%m-%d_%H%M%S)
-    artist=${cam}
+    # echo cam = $cam
+    #artist=${cam}
 
-    cd ${picdir}/${cam}/$(newdaydir)
-    echo "curl and store image to $PWD"
+    thiscam=${picdir}/${cam}
+    todaydir=$(newdaydir ${thiscam})
+    cd ${todaydir}
+    #echo ${picdir}
+    #echo ${cam}
+    #echo ${todaydir}
+    #echo "curl and store image to $PWD"
     curl -sO ${cam}/image.jpg
-    # echo 'curl returned ' $?
+    #echo 'curl returned ' $?
     
     # weed out nightshots
     mean=$(identify -format %[mean] image.jpg | sed s/[.].*//)
-    # echo "Mean is ${mean}"
-    if [ ${mean} -lt ${threshold} ] ; then
-        #  echo "Deleting night shot (mean=${mean})"
+    # echo Mean is ${mean}
+    if [[ ${mean} < ${threshold} ]] ; then
+        # echo 'Deleting night shot (mean='${mean}')'
         rm image.jpg
+        # make a timelapse movie if this is the first darkshot
+        if [ -e darkmarker ]; then 
+            makemovie ${cam} $(date +%Y-%m-%d)
+        fi
+        touch darkmarker
         continue
-    #else
-        # echo "daytime!"
     fi
     
-    if [ ${cam} -eq "carport" ] ; then
+    if [[ ${cam} == "carport" ]] ; then
         convert image.jpg -rotate 180 image.jpg
     fi
-    mv image.jpg timestamp.jpg
+    mv image.jpg ${timestamp}.jpg
 done
-
