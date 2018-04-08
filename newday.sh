d9a60c56d3dd3cb974aae01c2f0f1b6990cc4f08
St33v <github@f3rr3t.com>
2018-04-08 19:39:32 +1000

    robuster mkdir

--- newday.sh
+++ newday.sh
@@ -3,18 +3,18 @@
 
 function newdaydir {
     thisdir=${1}
-	yr=$(date +%Y)     # 4-digit year
-	mo=$(date +%m)
-	day=$(date +%d)
-	today=${thisdir}/${yr}/${mo}/${day}
+    yr=$(date +%Y)     # 4-digit year
+    mo=$(date +%m)
+    day=$(date +%d)
+    today=${thisdir}/${yr}/${mo}/${day}
     if [ ! -d ${today} ]; then
-		mkdir -p ${today}
-	fi
-	echo ${today}
+	mkdir -p ${today}
+    fi
+    echo ${today}
 }
 
 function makemovie {
-    mkdir mov
+    mkdir -p mov
     find -name "*.jpg" | sort | gawk 'BEGIN{a=1}{printf "cp %s mov/%04d.jpg\n", $0, a++}' | bash
     ffmpeg -r 24 -i mov/%04d.jpg ${cam}$(date +%Y-%m-%d).mp4
     touch /home/st33v/cams/${cam}moviemark
