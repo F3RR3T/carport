7c8a316d488fced726f1d265f4d1e8d928d280fd
st33v <github@f3rr3t.com>
2018-04-09 10:46:16 +1000

    removed makemovie function

--- newday.sh
+++ newday.sh
@@ -12,14 +12,3 @@ function newdaydir {
     fi
     echo ${today}
 }
-
-function makemovie {
-    mkdir -p mov
-    find -name "*.jpg" | sort | gawk 'BEGIN{a=1}{printf "cp %s mov/%04d.jpg\n", $0, a++}' | bash
-    movie=${cam}$(\date +%Y-%m-%d).mp4
-    ffmpeg -r 24 -i mov/%04d.jpg ${movie}
-    rm mov/*.jpg
-    rmdir mov
-    mv ${movie} /home/st33v/cams/${cam}/.
-    touch /home/st33v/cams/${cam}moviemark
-}
