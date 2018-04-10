1f3c466c9ec2b00b46eb355e4b8e4c04bc1f3315
st33v <github@f3rr3t.com>
2018-04-10 22:25:55 +1000

    more fixin

--- makemovie.sh
+++ makemovie.sh
@@ -12,7 +12,7 @@ picdir=${thisdir}/mov   # kludge until i rename var in paths.config
 # Function to make a timelapse movie after sunset
 function MakeMovie {
     framerate=${1:-24}  # default 24 frames per second
-    echo "MakeMovie started at $(date)"
+    echo "MakeMovie started at $(\date)"
     mkdir -p mov        # temp directory
     find -name "*.jpg" | sort | gawk 'BEGIN{a=1}{printf "cp %s mov/%04d.jpg\n", $0, a++}' | bash
     movie=$(\date +%Y-%m-%d)_${cam}.mp4
@@ -20,28 +20,34 @@ function MakeMovie {
     rm mov/*.jpg
     rmdir mov
     mv ${movie} ${picdir}/staging/.
-    echo "MakeMovie finished at $(date)"
+    echo "MakeMovie finished at $(\date)"
 }
 
 
-cd ${picdir}
-echo before looping: picdir: ${picdir}
-# when a cam senses sunset it writes a 'mark' file here
+cd ${picdir} 
+# when a cam (takeminicampix.sh) senses sunset it writes a 'mark' file here
 # The name of the markfile is in the format: "sunset-<camname>.mark"
+# The file contains the path to the day's images.
 for markfile in *.mark
 do
-    cam=${markfile#sunset-}     #strip prefix
-    cam=${cam%.mark}            #strip extension
+    if [ ${#markfile[@]} -eq 0 ]; then 
+        echo "No markfiles to process; stopping"
+        exit 0
+    fi
     if [[ ${markfile} == '*.mark' ]] ; then
         echo "Error: no markfiles therefore exiting."
         exit 1
     fi
+    cam=${markfile#sunset-}     #strip prefix
+    cam=${cam%.mark}            #strip extension
     echo markfile=${markfile}  cam=${cam}
     sourcedir=$(cat ${markfile})
     cd ${sourcedir}
     echo sourcedir ${sourcedir}
     if [[ $PWD == "/home/st33v" ]] ; then 
-        echo "Error: bad sourcedir"
+        echo "Error: bad sourcedir: ${sourcedir}"
+        cd ${picdir}   # reset for next iteration
+        rm ${markfile}
         continue 
     fi
     MakeMovie
@@ -49,9 +55,8 @@ do
     rm ${markfile}
 
     # upload
-    echo "Uploading ${movie} to ${web} started at $(date)."
+    echo "Uploading ${movie} to ${web} started at $(\date)."
     scp staging/${movie} ${web}/mov/.
-    # rm staging/{movie}
-    echo "Uploading finished at $(date)."
-
+    rm staging/${movie}
+    echo "Uploading finished at $(\date)."
 done
